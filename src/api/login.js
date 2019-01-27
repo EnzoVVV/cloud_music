ff7b4fa9f2026f7d5a0660c834667fff18cebbfd
EnzoVVV <mytitanic@163.com>
2019-01-27 19:48:13 +0800

    fix: 左侧菜单栏可以查看"我的好友"

--- src/api/login.js
+++ src/api/login.js
@@ -2,6 +2,7 @@
 
 import { HOST } from './config'
 import axios from 'axios'
+import { success } from './shared'
 
 export function login() {
     const url = HOST + `/login/cellphone?phone=18123683717&password=cloudtest`
@@ -12,5 +13,15 @@ export function login() {
 
 export function loginStatus() {
     const url = HOST + '/login/status'
-    return axios.get(url)
+    return axios.get(url).then(res => {
+        if(success(res.status)) {
+            const profile = res.data.profile
+            return {
+                id: profile.userId,
+                name: profile.nickname,
+                picUrl: profile.avatarUrl
+            }
+        }
+        return null
+    })
 }
--- src/components/left-menu/left-menu.vue
+++ src/components/left-menu/left-menu.vue
@@ -1,78 +1,81 @@
 <template>
-    <transition name='slide'>
-        <div v-show='showLeftMenu' class='left-menu'>
-            <div class='background' @click='hide' ref='bg'>
-            </div>
-            <div class='menu' @touchstart='touchstart' @touchmove='touchmove' @touchend='touchend' ref='menu'>
-                <scroll class='scroll' ref='scroll' :listen-scroll='listenScroll' :probe-type='probeType'>
-                    <div>
-                        <div class='header'>
+    <div>
+        <div class='background' @click='hide' ref='bg'>
+        </div>
+        <div class='menu' @touchstart='touchstart' @touchmove='touchmove' @touchend='touchend' ref='menu'>
+            <scroll class='scroll' ref='scroll' :listen-scroll='listenScroll' :probe-type='probeType'>
+                <div>
+                    <div class='header' ref='header'>
+                        <div class='header-wrapper'>
+                            <img class='avatar' :src='userInfo.picUrl'></img>
                             <div class='info'>
-                                <span>匿名用户</span>
+                                <span @click='showHomepage'>{{userInfo.name}}</span>
                             </div>
                         </div>
-                        <ul>
-                            <li class='line'>
-                                <IconSvg icon-class='setting'></IconSvg>
-                                <span>我的消息</span>
-                            </li>
-                            <li class='line'>
-                                <IconSvg icon-class='setting'></IconSvg>
-                                <span>我的会员</span>
-                            </li>
-                            <li class='line'>
-                                <IconSvg icon-class='setting'></IconSvg>
-                                <span>我的消息</span>
-                            </li>
-                            <li class='line'>
-                                <IconSvg icon-class='setting'></IconSvg>
-                                <span>我的消息</span>
-                            </li>
-                            <li class='line'>
-                                <IconSvg icon-class='setting'></IconSvg>
-                                <span>我的消息</span>
-                            </li>
-                            <li class='line'>
-                                <IconSvg icon-class='setting'></IconSvg>
-                                <span>我的消息</span>
-                            </li>
-                            <li class='line'>
-                                <IconSvg icon-class='setting'></IconSvg>
-                                <span>我的消息</span>
-                            </li>
-                            <li class='line'>
-                                <IconSvg icon-class='setting'></IconSvg>
-                                <span>我的消息</span>
-                            </li>
-                            <li class='line'>
-                                <IconSvg icon-class='setting'></IconSvg>
-                                <span>我的消息</span>
-                            </li>
-                        </ul>
                     </div>
-                    </scroll>
-                    <div class='bottom'>
-                        <div class='item'>
-                            <IconSvg icon-class='night-mode'></IconSvg>
-                            <span>夜间模式</span>
-                        </div>
-                        <div class='item'>
+                    <ul>
+                        <li class='line'>
                             <IconSvg icon-class='setting'></IconSvg>
-                            <span>设置</span>
-                        </div>
-                        <div class='item'>
-                            <IconSvg icon-class='exit'></IconSvg>
-                            <span>退出</span>
-                        </div>
-                    </div>
+                            <span>我的消息</span>
+                        </li>
+                        <li class='line'>
+                            <IconSvg icon-class='setting'></IconSvg>
+                            <span>我的会员</span>
+                        </li>
+                        <li class='line' @click='showFollow'>
+                            <IconSvg icon-class='setting'></IconSvg>
+                            <span>我的好友</span>
+                        </li>
+                        <li class='line'>
+                            <IconSvg icon-class='setting'></IconSvg>
+                            <span>我的消息</span>
+                        </li>
+                        <li class='line'>
+                            <IconSvg icon-class='setting'></IconSvg>
+                            <span>我的消息</span>
+                        </li>
+                        <li class='line'>
+                            <IconSvg icon-class='setting'></IconSvg>
+                            <span>我的消息</span>
+                        </li>
+                        <li class='line'>
+                            <IconSvg icon-class='setting'></IconSvg>
+                            <span>我的消息</span>
+                        </li>
+                        <li class='line'>
+                            <IconSvg icon-class='setting'></IconSvg>
+                            <span>我的消息</span>
+                        </li>
+                        <li class='line'>
+                            <IconSvg icon-class='setting'></IconSvg>
+                            <span>我的消息</span>
+                        </li>
+                    </ul>
+                </div>
+            </scroll>
+            <div class='bottom'>
+                <div class='item'>
+                    <IconSvg icon-class='night-mode'></IconSvg>
+                    <span>夜间模式</span>
+                </div>
+                <div class='item'>
+                    <IconSvg icon-class='setting'></IconSvg>
+                    <span>设置</span>
+                </div>
+                <div class='item'>
+                    <IconSvg icon-class='exit'></IconSvg>
+                    <span>退出</span>
                 </div>
             </div>
         </div>
-    </transition>
+    </div>
 </template>
 <script>
     import { scrollMixin } from 'common/js/mixins'
     import { translate, changeOpacity, opacity } from 'common/js/dom'
+    import { loginStatus } from 'api/login'
+    import { getUserInfo } from 'api/user'
+    import { mapGetters, mapMutations} from 'vuex'
     export default {
         name: 'leftmenu',
         mixins: [scrollMixin],
@@ -84,15 +87,45 @@
         data() {
             return {
                 showLeftMenu: false,
-                touch: {}
+                touch: {},
+                userInfo: {
+                    picUrl: 'static\images\default-avatar.png',
+                    name: '未登录'
+                }
             }
         },
         computed: {
-
+            ...mapGetters([
+                'loginUser'
+            ])
         },
         watch: {
         },
         methods: {
+            showHomepage() {
+                if(!this.login) {
+                    return
+                }
+                this.fold()
+                setTimeout(() => {
+                    this.showComponent('homepage', this.userInfo.id, true)
+                    this.hide()
+                }, 300)
+            },
+            showFollow() {
+                this.fold()
+                this.setHomepage(this.userInfo)
+                setTimeout(() => {
+                    this.showComponent('follow', '我的好友')
+                    this.hide()
+                }, 300)
+            },
+            fold() {
+                translate(this.$refs.menu, -100, 0, {
+                    transitionDuration: '0.3s',
+                    percent: true
+                })
+            },
             hide() {
                 this.$emit('hide')
             },
@@ -135,24 +168,56 @@
                     }
                 }
                 this.touch = {}
-            }
+            },
+            getUserInfo() {
+                loginStatus().then(res => {
+                    if(res) {
+                        this.userInfo = res
+                        this.getUserBG()
+                        this.login = true
+                    }
+                })
+            },
+            getUserBG() {
+                getUserInfo(this.userInfo.id).then(res => {
+                    this.userInfo.bgUrl = res.bgUrl
+                    this.setUser(this.userInfo)
+                    this.setBG()
+                })
+            },
+            setBG() {
+                this.$nextTick(() => {
+                    this.$refs.header.style.backgroundImage = `url(${this.userInfo.bgUrl}`
+                }) 
+            },
+            ...mapMutations({
+                setUser: 'SET_LOGIN_USER',
+                setHomepage: 'SET_HOMEPAGE'
+            })
         },
         created() {
-
+            if(!this.loginUser) {
+                this.getUserInfo()
+            } else {
+                this.userInfo = this.loginUser
+                this.login = true
+                if(this.userInfo.bgUrl) {
+                    this.setBG()
+                }
+            }
         },
         mounted() {
             this.showLeftMenu = true
             this.maxMoveDistance = window.innerWidth * 0.85
+            translate(this.$refs.menu, 0, 0, {
+                transitionDuration: '0.3s',
+                percent: true
+            })
         }
     }
 </script>
 <style lang='stylus' scoped>
     @import '~common/stylus/variable'
-    &.slide-enter-active, &.slide-leave-active
-        transition: all 0.3s
-    &.slide-enter, &.slide-leave-to
-        .background .menu
-            transform: translate3d(-100%, 0, 0)
     .background
         position: fixed
         top: 0
@@ -161,14 +226,15 @@
         bottom: 0
         background: $color-text
         opacity: 0.5
-        z-index: 2900
+        z-index: 5900
     .menu
         position: fixed
         left: 0
         top: 0
         right: 15%
         bottom: 0
-        z-index: 3000
+        z-index: 6000
+        transform: translate3d(-100%, 0, 0)
         background: $color-theme-l
         overflow: hidden
         .scroll
@@ -180,14 +246,20 @@
             overflow: hidden
             .header
                 width: 100%
-                height: 200px
+                height: 130px
                 overflow: hidden
-                background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJIFfomYGWHkuqnOuyO_rHnHZI5-yBB5NpKwzZ97CBDIGm0RHdLQ')
+                background-image: 'static\images\default-bg.png'
                 position: relative
-                .info
+                &-wrapper
                     position: absolute 
                     left: 15px
                     bottom: 15px
+                    width: 100%
+                    .avatar
+                        width: 60px
+                        height: 60px
+                        padding-bottom: 15px
+                        border-radius: 50%
             .line
                 padding-left: 5px
                 line-height: 44px
@@ -208,7 +280,6 @@
             justify-content: center
             .item
                 flex: 1
-                margin: 0 10px
                 display: flex
                 justify-content: center
                 align-items: center
